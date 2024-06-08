66d2756d7e34bd29f88931dfa7e5b866c5b846ed
YunDaeHyeon <kayuaao96@gmail.com>
2024-06-08 23:39:50 +0900

    feat: 라우팅 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,57 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // 부모 컴포넌트 지정
+    element: <Layout/>,
+    // 라우트(자식) 컴포넌트 지정
+    children: [
+      {
+        path: "",
+        element: <Home/>,
+      },
+      {
+        path: "profile",
+        element: <Profile/>,
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount/>
+  }
+])
+
+// 전역 styled-components 정의
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 function App() {
   return (
     <>
+      <GlobalStyles/>
+      <RouterProvider router={router}/>
     </>
   )
 }

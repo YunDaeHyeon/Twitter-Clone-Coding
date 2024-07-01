828bbc4ae7336a98e70191a32a80c05ff5c4e6b0
YunDaeHyeon <kayuaao96@gmail.com>
2024-07-01 15:19:33 +0900

    feat: 트윗 폼 추가

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,12 @@
-import { auth } from "../firebase";
+import styled from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 export default function Home(){
-    console.log("현재 접속중인 사용자 : ", auth.currentUser?.email);
-    const logOut = () => {
-        // 현재 접속중인 세션을 로그아웃 시킨다.
-        auth.signOut();
-    }
     return (
-        <>
-            <h1><button onClick={logOut}>로그아웃</button></h1>
-        </>
+        <Wrapper>
+            <PostTweetForm/>
+        </Wrapper>
     );
 }

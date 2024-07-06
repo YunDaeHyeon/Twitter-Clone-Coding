6038001851465703afda9d16a2472c300f099f35
YunDaeHyeon <kayuaao96@gmail.com>
2024-07-06 18:28:49 +0900

    feat: 트윗 호출 리스너 추가

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,67 +1,85 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  Unsubscribe,
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { database } from "../firebase";
 import Tweet from "./tweet";
 
 // Tweet의 타입을 정의
-export interface ITweet{
-    id: string;
-    // "?" 는 해당 변수가 반드시 있을 필요 없다라는 의미
-    photo?: string;
-    tweet: string;
-    userId: string;
-    username: string;
-    createdAt: number;
+export interface ITweet {
+  id: string;
+  // "?" 는 해당 변수가 반드시 있을 필요 없다라는 의미
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
 }
 
 const Wrapper = styled.div`
-    
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
 `;
 
-export default function Tiemline(){
-    // 트윗 데이터를 저장할 상태를 정의
-    const [tweets, setTweets] = useState<ITweet[]>([]);
+const TWEET_LIMIT = 25;
 
+export default function Tiemline() {
+  // 트윗 데이터를 저장할 상태를 정의
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+
+  // 컴포넌트가 마운트 될 때 트윗 데이터 호출
+  useEffect(() => {
+    // Unsubscribe 혹은 null를 가진다.
+    let unsubscribe: Unsubscribe | null = null;
     // firestore에서 트윗 데이털르 가져오는 비동기 처리
-    const fetchTweets = async() => {
+    const fetchTweets = async () => {
+      // firestore에서 "tweets" 이라는 collection을 호출하여 "createdAt" 필드 기준으로
+      // 내림차순(desc) 정렬. (오름차순 = asc)
+      const tweetsQuery = query(
+        collection(database, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(TWEET_LIMIT) // 25개의 collection만 호출
+      );
 
-        // firestore에서 "tweets" 이라는 collection을 호출하여 "createdAt" 필드 기준으로
-        // 내림차순(desc) 정렬. (오름차순 = asc)
-        const tweetsQuery = query(
-            collection(database, "tweets"),
-            orderBy("createdAt", "desc")
-        );
-        // 위에서 정의한 쿼리를 실행시켜 데이터를 호출
-        const spanshot = await getDocs(tweetsQuery);
-        // 스냅샷에서 각 document를 트윗 객체로 전환
-        const tweets = spanshot.docs.map(doc => {
-            // firestore document에서 각 트윗 데이터를 구조분해 할당
-            const {tweet, createdAt, userId, username, photo} = doc.data();
-            return{
-                tweet,      // 트윗 내용 
-                createdAt,  // 생성 시간
-                userId,     // 작성자 ID
-                username,   // 작성자 이름
-                photo,      // 트윗에 첨부된 사진
-                id: doc.id  // 트윗의 문서 ID (고유 식별자, document ID = tweet ID)
-            }
+      // tweetsQuery에 리스너 추가
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        // firestore에 있는 document에 실시간으로 변동될 시 해당 document를 array로 반환
+        const tweets = snapshot.docs.map((doc) => {
+          // firestore document에서 각 트윗 데이터를 구조분해 할당
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet, // 트윗 내용
+            createdAt, // 생성 시간
+            userId, // 작성자 ID
+            username, // 작성자 이름
+            photo, // 트윗에 첨부된 사진
+            id: doc.id, // 트윗의 문서 ID (고유 식별자, document ID = tweet ID)
+          };
         });
-
         // 생성한 트윗 객체를 상태로 저장
         setTweets(tweets);
-    }
-    
-    // 컴포넌트가 마운트 될 때 트윗 데이터 호출
-    useEffect(() => {
-        fetchTweets();
-    },[]); // 빈 배열 = 컴포넌트가 처음 마운트될 때 실행
+      });
+    };
+    fetchTweets();
+    // useEffect의 clean up
+    return () => {
+      // unsubscribe가 null이 아니면 호출 (리스너 구독 취소)
+      unsubscribe && unsubscribe();
+    };
+  }, []); // 빈 배열 = 컴포넌트가 처음 마운트될 때 실행
 
-    return(
-        <Wrapper>
-            {tweets.map((tweet) => (
-                <Tweet key={tweet.id} {...tweet}/>
-            ))}
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
 }
--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -2,46 +2,44 @@ import styled from "styled-components";
 import { ITweet } from "./timeline";
 
 const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: 3fr 1fr;
-    padding: 20px;
-    border: 1px solid rgba(255, 255, 255, 0.5);
-    border-radius: 15px;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  padding: 20px;
+  border: 1px solid rgba(255, 255, 255, 0.5);
+  border-radius: 15px;
 `;
 
 const Column = styled.div``;
 
 const Photo = styled.img`
-    width: 100px;
-    height: 100px;
-    border-radius: 15px;
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
 `;
 
 const Username = styled.span`
-    font-weight: 600;
-    font-size: 15px;
+  font-weight: 600;
+  font-size: 15px;
 `;
 
 const Payload = styled.p`
-    margin: 10px 0px;
-    font-size: 18px;
+  margin: 10px 0px;
+  font-size: 18px;
 `;
 
-export default function Tweet({ username, photo, tweet }:ITweet){
-    return(
-        <Wrapper>
-            <Column>
-                <Username>{username}</Username>
-                <Payload>{tweet}</Payload>
-            </Column>
-            {   
-                // 사진이 존재한다면 출력, 그렇지 않다면 공백
-                photo ? (
-                    <Column>
-                        <Photo src={photo}/>
-                    </Column>
-                ) : null
-            }
-        </Wrapper>
-    )
+export default function Tweet({ username, photo, tweet }: ITweet) {
+  return (
+    <Wrapper>
+      <Column>
+        <Username>{username}</Username>
+        <Payload>{tweet}</Payload>
+      </Column>
+      <Column>
+        {
+          // 사진이 존재한다면 출력, 그렇지 않다면 공백
+          photo ? <Photo src={photo} /> : null
+        }
+      </Column>
+    </Wrapper>
+  );
 }
